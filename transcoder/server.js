0564738a391f9e7aa73224cd2c4822b42b9ab12d
Vincent <mail@vincentmoonen.com>
2021-12-19 16:46:13 +0100

    Now everything gets proxied through Node process

--- transcoder/server.js
+++ transcoder/server.js
@@ -6,23 +6,35 @@ const path = require('path')
 const tmp = require('tmp')
 const cors = require('cors')
 const { spawn } = require('child_process')
+const url = require('url')
 
 const upstream = 'http://localhost:8080'
+const upstreamHostname = 'localhost'
+const upstreamPort = 8080
 
 const app = express()
 app.use(cors())
 const port = 8081
 
-// Handle all requests that come through
-app.get('*', (req, res) => {
+function handleSegmentRequest(req, res) {
+    console.log(`Handling segment request ${req.url}`)
+    let path = url.parse(req.url).pathname
     // Open temp file
     const tempFile = tmp.tmpNameSync() + '.ts'
     const fd = fs.openSync(tempFile, 'w')
     fs.closeSync(fd)
     // Open write stream
     const stream = fs.createWriteStream(tempFile)
+    const options = {
+        hostname: upstreamHostname,
+        port: upstreamPort,
+        path: path,
+        headers: {
+            Host: req.get("Host")
+        }
+    }
     // Request the resource
-    const request = http.get(upstream + req.url, function(response) {
+    const request = http.get(options, function(response) {
         // Pipe the response to the file
         response.pipe(stream)
         // When done
@@ -52,6 +64,47 @@ app.get('*', (req, res) => {
             })
         })
     })
+}
+
+function handleMasterRequest(req, res) {
+    console.log(`Handling Master request ${req.url}`)
+    console.log(req.get("Host"))
+    let path = url.parse(req.url).pathname
+    const options = {
+        hostname: upstreamHostname,
+        port: upstreamPort,
+        path: path,
+        headers: {
+            Host: req.get("Host")
+        }
+    }
+    http.get(options, (response) => response.pipe(res))
+}
+
+function handleIndexRequest(req, res) {
+    console.log(`Handling Index request ${req.url}`)
+    let path = url.parse(req.url).pathname
+    const options = {
+        hostname: upstreamHostname,
+        port: upstreamPort,
+        path: path,
+        headers: {
+            Host: req.get("Host")
+        }
+    }
+    http.get(options, (response) => response.pipe(res))
+}
+
+// Handle all requests that come through
+app.get('*', (req, res) => {
+    let path = url.parse(req.url).pathname
+    if (path.endsWith('.ts')) {
+        handleSegmentRequest(req, res)
+    } else if (path.endsWith('master.m3u8')) {
+        handleMasterRequest(req, res)
+    } else if (path.endsWith('index-v1-a1.m3u8')) {
+        handleIndexRequest(req, res)
+    }
 })
 
 // Start the server
